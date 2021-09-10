321e788f38a7514d93861d8d33d02b73dcd8e903
Simon Naumov <daretoodefy@gmail.com>
2021-09-10 22:33:06 +0300

    :poop::beer: reverting poopbeer changes

--- src/main/kotlin/com/github/pool_party/resistance_bot/state/Board.kt
+++ src/main/kotlin/com/github/pool_party/resistance_bot/state/Board.kt
@@ -7,7 +7,7 @@ class Board(val resistanceNumber: Int, val spyNumber: Int, val missionAgentNumbe
     val capacity = resistanceNumber + spyNumber
 
     init {
-//        check(resistanceNumber > 0 && spyNumber > 0)
+        check(resistanceNumber > 0 && spyNumber > 0)
         check(missionAgentNumber.size == Configuration.WIN_NUMBER * 2 - 1)
         check(missionAgentNumber.all { it in 0..(capacity - spyNumber + 1) })
     }
